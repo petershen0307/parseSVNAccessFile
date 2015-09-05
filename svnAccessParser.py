56a437c205d7a30153f873718da3d100bd4a7e8e
Peter Shen <petershen0307@hotmail.com>
2015-09-05 23:21:28 +0800

    remove redundant code and repository group dict for next step

--- svnAccessParser.py
+++ svnAccessParser.py
@@ -4,17 +4,17 @@ import collections
 if '__main__' == __name__:
     svnConfig = configparser.ConfigParser()
     svnConfig.read('svnaccess.apache')
-    user_name_dict = collections.defaultdict(list)
+    user_name_dict = collections.defaultdict(list) # key: user, value: repository groups
+    repository_group_dict = collections.defaultdict(dict) # key: repository group, value: {repo:, permission:}
     for section in svnConfig.sections():
         print(section)
         for item_of_section in svnConfig.items(section):
             if 'groups' == section:
                 repository_group_name = item_of_section[0]
                 print(repository_group_name)
-                for user_list in item_of_section[1:]:
-                    for user in user_list.split(','):
-                        user_name_dict[user].append(repository_group_name)
-                        # in an other option: dict.setdefault(key,[]).append(value)
-                        print(user, end=',')
+                for user in item_of_section[1].split(','):
+                    user_name_dict[user].append(repository_group_name)
+                    # in an other option: dict.setdefault(key,[]).append(value)
+                    print(user, end=',')
                 print()
     print(user_name_dict['cshuang'])
